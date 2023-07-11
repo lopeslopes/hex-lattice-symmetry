5418c9e28840542c7a827bf14e832edbce2c5d40
Mateus Lopes <mateuslmazziero@gmail.com>
2023-07-11 18:23:55 -0300

    Added new type of lattice

--- 2d_lattice.f90
+++ 2d_lattice.f90
@@ -1,32 +1,81 @@
 program symmetry
 implicit none
 
-real*8, dimension(:,:), allocatable :: lat1, lat2
+real*8, dimension(:,:), allocatable :: lat1, lat2, lat3, lat1f, lat2f
+real*8, dimension(:,:), allocatable :: lat1_aux, lat2_aux, lat3_aux
 real*8                              :: rot_angle, lat_separation, lat_const
 real*8                              :: pi
-integer                             :: i, num_points
+integer                             :: i, j, k, l, num_points
+logical                             :: cond1, cond2
 
 pi = 4.d0*atan(1.d0)
 
-num_points      = 10000
+num_points      = 4000000
 lat_separation  = 10.d0
-lat_const       = 1.d0
-rot_angle       = (10.d0/180.d0) * pi
+lat_const       = 2.42d0
+rot_angle       = (1.08445d0/180.d0) * pi
 
 allocate(lat1(num_points, 3))
 allocate(lat2(num_points, 3))
 lat1 = 0.d0
 lat2 = 0.d0
 
-call create_square_lattice(lat1, 0.d0          , lat_const, 100)
-call create_square_lattice(lat2, lat_separation, lat_const, 100)
+call create_lattice_eh(lat1, 0.d0          , lat_const, 2000)
+call create_lattice_eh(lat2, lat_separation, lat_const, 2000)
 
+allocate(lat1_aux(num_points,3))
+allocate(lat2_aux(num_points,3))
+lat1_aux = 0.d0
+lat2_aux = 0.d0
+j = 0
+k = 0
 do i=1, num_points
     lat2(i,:) = rotate_point(lat2(i,:), [0.d0, 0.d0, lat_separation], rot_angle)
 enddo
 
-call write_lattice(lat1, "lattice01.dat")
-call write_lattice(lat2, "lattice02.dat")
+do i=1, num_points
+    cond1 = (lat1(i,1) > -50.d0) .and. (lat1(i,1) < 50.d0) .and. (lat1(i,2) >= 0.d0)
+    cond2 = (lat2(i,1) > -50.d0) .and. (lat2(i,1) < 50.d0) .and. (lat2(i,2) >= 0.d0)
+    if (cond1) then
+        j = j + 1
+        lat1_aux(j,:) = lat1(i,:)
+    endif
+    if (cond2) then
+        k = k + 1
+        lat2_aux(k,:) = lat2(i,:)
+    endif
+enddo
+
+allocate(lat1f(j,3))
+allocate(lat2f(k,3))
+lat1f = lat1_aux(1:j,:)
+lat2f = lat2_aux(1:k,:)
+
+call write_lattice(lat1f, "lattice01.dat")
+call write_lattice(lat2f, "lattice02.dat")
+
+allocate(lat3_aux(num_points,3))
+lat3_aux = 0.d0
+l = 0
+do i=1, min(j,k)
+    if (exists_in_lattice([lat2f(i,1), lat2f(i,2), 0.d0], lat1f(i-1000:i+1000,:))) then
+        l = l + 1
+        lat3_aux(l,:) = lat2f(i,:)
+    endif
+enddo
+
+allocate(lat3(l,3))
+lat3 = lat3_aux(1:l,:)
+call write_lattice(lat3, "lattice03.dat")
+
+deallocate(lat1)
+deallocate(lat2)
+deallocate(lat3)
+deallocate(lat1f)
+deallocate(lat2f)
+deallocate(lat1_aux)
+deallocate(lat2_aux)
+deallocate(lat3_aux)
 
 contains
 
@@ -40,7 +89,7 @@ function exists_in_lattice(point, lattice) result(flag)
 
     allocate(condition_list(size(lattice,1)))
 
-    tol = 1e-12
+    tol = 1e-8
     do i=1, size(lattice, 1)
         c1 = abs(lattice(i,1) - point(1))
         c2 = abs(lattice(i,2) - point(2))
@@ -52,69 +101,7 @@ function exists_in_lattice(point, lattice) result(flag)
     flag = any(condition_list .eqv. .true.)
 end function
 
-subroutine create_lattice(lattice, z, a)
-    real*8, dimension(:,:), intent(inout) :: lattice
-    real*8, intent(in)                    :: z, a
-    real*8, dimension(3)                  :: p_aux
-    real*8                                :: angle_rad, pi
-    integer                               :: i, ind
-
-    print*, "Creating lattice"
-
-    pi = 4.d0 * atan(1.d0)
-    lattice(:,3) = z
-    lattice(1,:) = [0.d0, 0.d0, z]
-    i = 2
-    ind = 2
-
-    do while(ind < size(lattice, 1))
-        p_aux = lattice(i-1,:) + [a, 0.d0, 0.d0]
-        if (.not. exists_in_lattice(p_aux, lattice)) then
-            lattice(ind,:) = p_aux
-            ind = ind + 1
-        endif
-        
-        angle_rad = (60.d0/180.d0) * pi
-        p_aux = rotate_point(lattice(i,:), lattice(i-1,:), angle_rad)
-        if (.not. exists_in_lattice(p_aux, lattice)) then
-            lattice(ind,:) = p_aux
-            ind = ind + 1
-        endif
-        
-        angle_rad = (120.d0/180.d0) * pi
-        p_aux = rotate_point(lattice(i,:), lattice(i-1,:), angle_rad)
-        if (.not. exists_in_lattice(p_aux, lattice)) then
-            lattice(ind,:) = p_aux
-            ind = ind + 1
-        endif
-
-        angle_rad = (180.d0/180.d0) * pi
-        p_aux = rotate_point(lattice(i,:), lattice(i-1,:), angle_rad)
-        if (.not. exists_in_lattice(p_aux, lattice)) then
-            lattice(ind,:) = p_aux
-            ind = ind + 1
-        endif
-
-        angle_rad = (240.d0/180.d0) * pi
-        p_aux = rotate_point(lattice(i,:), lattice(i-1,:), angle_rad)
-        if (.not. exists_in_lattice(p_aux, lattice)) then
-            lattice(ind,:) = p_aux
-            ind = ind + 1
-        endif
-
-        angle_rad = (300.d0/180.d0) * pi
-        p_aux = rotate_point(lattice(i,:), lattice(i-1,:), angle_rad)
-        if (.not. exists_in_lattice(p_aux, lattice)) then
-            lattice(ind,:) = p_aux
-            ind = ind + 1
-        endif
-
-        i = i + 1
-    enddo
-    lattice(1,:) = [0.d0, 0.d0, z]
-end subroutine
-
-subroutine create_square_lattice(lattice, z, a, num_columns)
+subroutine create_lattice_bch(lattice, z, a, num_columns) ! body-centered hexagon
     real*8, dimension(:,:), intent(inout) :: lattice
     real*8, intent(in)                    :: z, a
     real*8, dimension(3)                  :: v1, v2, origin_gen, lat_origin
@@ -141,6 +128,53 @@ subroutine create_square_lattice(lattice, z, a, num_columns)
             origin_gen = origin_gen + v1
         endif
         origin_gen = origin_gen + v2
+    enddo
+    lattice(1,:) = [0.d0, 0.d0, z]
+
+    i0 = ((row/2) * num_columns) + (num_columns/2)
+    lat_origin = lattice(i0,:)
+    lattice(:,1) = lattice(:,1) - lat_origin(1)
+    lattice(:,2) = lattice(:,2) - lat_origin(2)
+    lattice(:,3) = lattice(:,3) - lat_origin(3)
+end subroutine
+
+subroutine create_lattice_eh(lattice, z, a, num_columns) ! empty hexagon
+    real*8, dimension(:,:), intent(inout) :: lattice
+    real*8, intent(in)                    :: z, a
+    real*8, dimension(3)                  :: v1, v2, v3, v4, origin_a, origin_b, lat_origin
+    real*8                                :: angle, pi, d1
+    integer                               :: i, j, num_columns, row, i0
+
+    pi = 4.d0 * atan(1.d0)
+    angle = (30.d0/180.d0) * pi
+    d1 = a/sqrt(3.d0)
+
+    v1 = [1.5d0*d1, 0.d0, 0.d0]
+    v2 = [a*cos(angle), a*sin(angle), 0.d0]
+    v3 = [0.d0, a, 0.d0]
+
+    origin_a = [0.d0, 0.d0, z]
+    origin_b = origin_a + [d1, 0.d0, 0.d0]
+
+    row = 1
+    lattice(1,:) = origin_a
+    lattice(2,:) = origin_b
+    i = 3
+    do while(i < size(lattice,1))
+        do j=1, num_columns, 2
+            lattice(i,:) = origin_a + real(j)*v1
+            i = i + 1
+            lattice(i,:) = origin_b + real(j)*v1
+            i = i + 1
+        enddo
+        row  = row + 1
+        if (modulo(row,2) == 1) then
+            origin_a = origin_a + v2
+            origin_b = origin_b + v2
+        else
+            origin_a = origin_a - v2 + v3
+            origin_b = origin_b - v2 + v3
+        endif
     enddo
     lattice(1,:) = [0.d0, 0.d0, z]
 
--- plot_lattice.py
+++ plot_lattice.py
@@ -8,7 +8,6 @@ points1 = []
 for line in data1:
     aux = line.split(";")
     points1.append([float(aux[0]), float(aux[1]), float(aux[2])])
-
 points1 = np.array(points1)
 
 with open(file="lattice02.dat") as f:
@@ -18,16 +17,42 @@ points2 = []
 for line in data2:
     aux = line.split(";")
     points2.append([float(aux[0]), float(aux[1]), float(aux[2])])
-
 points2 = np.array(points2)
 
+with open(file="lattice03.dat") as f:
+    data3 = f.readlines()
 
-ax1 = plt.subplot(111, projection='3d')
-graf1 = ax1.scatter(points1[:,0], points1[:,1], points1[:,2])
-ax2 = plt.subplot(111, projection='3d')
-graf2 = ax2.scatter(points2[:,0], points2[:,1], points2[:,2])
+points3 = []
+for line in data3:
+    aux = line.split(";")
+    points3.append([float(aux[0]), float(aux[1]), float(aux[2])])
+points3 = np.array(points3)
 
-ax1.set_box_aspect((1,1,1/10))
-ax2.set_box_aspect((1,1,1/10))
+
+ax1 = plt.subplot(111)
+graf1 = ax1.scatter(points1[:,0], points1[:,1], s=5)
+ax2 = plt.subplot(111)
+graf2 = ax2.scatter(points2[:,0], points2[:,1], s=5)
+ax3 = plt.subplot(111)
+graf3 = ax3.scatter(points3[:,0], points3[:,1], s=5)
+
+ax1.set_aspect(1)
+ax2.set_aspect(1)
+ax3.set_aspect(1)
+
+ax1.set_xlim(-70, 70)
+ax1.set_ylim(0, 140)
+
+## 3D PLOT (TOO SLOW)
+# ax1 = plt.subplot(111, projection='3d')
+# graf1 = ax1.scatter(points1[:,0], points1[:,1], points1[:,2], s=1)
+# ax2 = plt.subplot(111, projection='3d')
+# graf2 = ax2.scatter(points2[:,0], points2[:,1], points2[:,2], s=1)
+# ax3 = plt.subplot(111, projection='3d')
+# graf3 = ax3.scatter(points3[:,0], points3[:,1], points3[:,2], s=1)
+
+# ax1.set_box_aspect((1,1,1/10))
+# ax2.set_box_aspect((1,1,1/10))
+# ax3.set_box_aspect((1,1,1/10))
 
 plt.show()
