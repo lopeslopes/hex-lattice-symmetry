92cf0136a7e981b6e790aa1049c58b782f83f1fb
Mateus Lopes <mateuslmazziero@gmail.com>
2023-07-24 17:16:52 -0300

    Changed method of generating and plotting AB lattices

--- 2d_lattice.f90
+++ 2d_lattice.f90
@@ -1,115 +1,85 @@
 program symmetry
 implicit none
 
-real*16, dimension(:,:), allocatable :: lat1A, lat1B, lat2A, lat2B, lat3
-real*16, dimension(:,:), allocatable :: lat1Af, lat1Bf, lat2Af, lat2Bf
-real*16, dimension(:,:), allocatable :: lat1A_aux, lat1B_aux, lat2A_aux, lat2B_aux, lat3_aux
+real*16, dimension(:,:), allocatable :: lat1, lat1f, lat1_aux, lat2, lat2f, lat2_aux, lat3, lat3_aux
 real*16                              :: rot_angle, lat_separation, lat_const
 real*16                              :: pi
-integer                              :: i, j, k, l, m, num_points
-logical                              :: cond1A, cond2A, cond1B, cond2B
-logical                              :: cond3AA, cond3AB, cond3BA, cond3BB
+integer                              :: i, j, k, l, num_points
+logical                              :: cond1A, cond2A, cond1B, cond2B, cond3
 
 ! INITIAL DEFINITIONS
 pi = 4.e0_16*atan(1.e0_16)
 
 num_points      = 4000000
 lat_separation  = 10.e0_16
-lat_const       = 2.42d0
-rot_angle       = (1.08445d0/180.e0_16) * pi
+lat_const       = 2.42e0_16
+rot_angle       = (1.08445e0_16/180.e0_16) * pi
 
 ! ALLOCATE THE TWO LATTICES
-allocate(lat1A(num_points/2, 3))
-allocate(lat1B(num_points/2, 3))
-allocate(lat2A(num_points/2, 3))
-allocate(lat2B(num_points/2, 3))
-lat1A = 0.e0_16
-lat1B = 0.e0_16
-lat2A = 0.e0_16
-lat2B = 0.e0_16
+allocate(lat1(num_points, 3))
+allocate(lat2(num_points, 3))
+lat1 = 0.e0_16
+lat2 = 0.e0_16
 
 ! GENERATE THE LATTICE POINTS
-call create_lattice_eh_AB(lat1A, lat1B, 0.e0_16       , lat_const, 1000)
-call create_lattice_eh_AB(lat2A, lat2B, lat_separation, lat_const, 1000)
+call create_lattice_eh(lat1, 0.e0_16       , lat_const, 2000)
+call create_lattice_eh(lat2, lat_separation, lat_const, 2000)
 
 ! ROTATE SECOND LATTICE
-do i=1, num_points/2
-    lat2A(i,:) = rotate_point(lat2A(i,:), [0.e0_16, 0.e0_16, lat_separation], rot_angle)
-    lat2B(i,:) = rotate_point(lat2B(i,:), [0.e0_16, 0.e0_16, lat_separation], rot_angle)
+do i=1, num_points
+    lat2(i,:) = rotate_point(lat2(i,:), [0.e0_16, 0.e0_16, lat_separation], rot_angle)
 enddo
 
-! TRIM THE LATTICES (ONLY USE WHAT HAS -50 < x < 50 AND y > 0) FOR PERFORMANCE
-allocate(lat1A_aux(num_points/2,3))
-allocate(lat2A_aux(num_points/2,3))
-allocate(lat1B_aux(num_points/2,3))
-allocate(lat2B_aux(num_points/2,3))
-lat1A_aux = 0.e0_16
-lat2A_aux = 0.e0_16
-lat1B_aux = 0.e0_16
-lat2B_aux = 0.e0_16
+!! TRIM THE LATTICES (ONLY USE WHAT HAS -50 < x < 50 AND y > 0) FOR PERFORMANCE
+allocate(lat1_aux(num_points,3))
+allocate(lat2_aux(num_points,3))
+lat1_aux = 0.e0_16
+lat2_aux = 0.e0_16
 
 j = 0
 k = 0
-l = 0
-m = 0
-do i=1, num_points/2
-    cond1A = (lat1A(i,1) > -50.e0_16) .and. (lat1A(i,1) < 50.e0_16) .and. (lat1A(i,2) >= 0.e0_16)
-    cond1B = (lat1B(i,1) > -50.e0_16) .and. (lat1B(i,1) < 50.e0_16) .and. (lat1B(i,2) >= 0.e0_16)
-    cond2A = (lat2A(i,1) > -50.e0_16) .and. (lat2A(i,1) < 50.e0_16) .and. (lat2A(i,2) >= 0.e0_16)
-    cond2B = (lat2B(i,1) > -50.e0_16) .and. (lat2B(i,1) < 50.e0_16) .and. (lat2B(i,2) >= 0.e0_16)    
-    if (cond1A) then
+do i=1, num_points
+    cond1A = (lat1(i,1) > -100.e0_16) .and. (lat1(i,1) < 100.e0_16)
+    cond1B = (lat1(i,2) <= 1000.e0_16) .and. (lat1(i,2) >= 0.e0_16)
+    cond2A = (lat2(i,1) > -100.e0_16) .and. (lat2(i,1) < 100.e0_16)
+    cond2B = (lat2(i,2) <= 1000.e0_16) .and. (lat2(i,2) >= 0.e0_16)
+
+    if (cond1A .and. cond1B) then
         j = j + 1
-        lat1A_aux(j,:) = lat1A(i,:)
+        lat1_aux(j,:) = lat1(i,:)
     endif
-    if (cond2A) then
+    if (cond2A .and. cond2B) then
         k = k + 1
-        lat2A_aux(k,:) = lat2A(i,:)
-    endif
-    if (cond1B) then
-        l = l + 1
-        lat1B_aux(j,:) = lat1B(i,:)
-    endif
-    if (cond2B) then
-        m = m + 1
-        lat2B_aux(k,:) = lat2B(i,:)
+        lat2_aux(k,:) = lat2(i,:)
     endif
 enddo
 
 ! STORE TRIMMED LATTICES IN SMALLER ARRAYS
-allocate(lat1Af(j,3))
-allocate(lat2Af(k,3))
-allocate(lat1Bf(l,3))
-allocate(lat2Bf(m,3))
-lat1Af = lat1A_aux(1:j,:)
-lat2Af = lat2A_aux(1:k,:)
-lat1Bf = lat1B_aux(1:l,:)
-lat2Bf = lat2B_aux(1:m,:)
+allocate(lat1f(j,3))
+allocate(lat2f(k,3))
+lat1f = lat1_aux(1:j,:)
+lat2f = lat2_aux(1:k,:)
 
+!! SECTION TO USE IF NOT TRIMMING THE LATTICES
+!allocate(lat1f(size(lat1,1), size(lat1,2)))
+!allocate(lat2f(size(lat2,1), size(lat2,2)))
+!lat1f = lat1
+!lat2f = lat2
 
 ! WRITE THEM TO FILES
-call write_lattice(lat1Af, "lattice1A.dat")
-call write_lattice(lat2Af, "lattice2A.dat")
-call write_lattice(lat1Bf, "lattice1B.dat")
-call write_lattice(lat2Bf, "lattice2B.dat")
+call write_lattice(lat1f, "lattice01.dat")
+call write_lattice(lat2f, "lattice02.dat")
 
 ! GENERATE THIRD LATTICE TO STORE WHICH POINTS OF THE FIRST ONE
 ! ARE EXACTLY BELOW A POINT FROM THE SECOND ONE
 allocate(lat3_aux(num_points,3))
 lat3_aux = 0.e0_16
 l = 0
-do i=1, min(j,k)
-    cond3AA = exists_in_lattice([lat2Af(i,1), lat2Af(i,2), 0.e0_16], lat1Af(i-2000:i+2000,:))
-    cond3AB = exists_in_lattice([lat2Af(i,1), lat2Af(i,2), 0.e0_16], lat1Bf(i-2000:i+2000,:))
-    cond3BA = exists_in_lattice([lat2Bf(i,1), lat2Bf(i,2), 0.e0_16], lat1Af(i-2000:i+2000,:))
-    cond3BB = exists_in_lattice([lat2Bf(i,1), lat2Bf(i,2), 0.e0_16], lat1Bf(i-2000:i+2000,:))
-
-    if (cond3AA .or. cond3AB) then
+do i=1, min(size(lat1f,1), size(lat2f,1))
+    cond3 = exists_in_lattice([lat2f(i,1), lat2f(i,2), 0.e0_16], lat1f(i-10000:i+10000,:))
+    if (cond3) then
         l = l + 1
-        lat3_aux(l,:) = lat2Af(i,:)
-    endif
-    if (cond3BA .or. cond3BB) then
-        l = l + 1
-        lat3_aux(l,:) = lat2Bf(i,:)
+        lat3_aux(l,:) = lat2f(i,:)
     endif
 enddo
 
@@ -119,19 +89,13 @@ lat3 = lat3_aux(1:l,:)
 call write_lattice(lat3, "lattice03.dat")
 
 ! DEALLOCATE EVERYTHING
-deallocate(lat1A)
-deallocate(lat2A)
-deallocate(lat1B)
-deallocate(lat2B)
+deallocate(lat1)
+deallocate(lat2)
 deallocate(lat3)
-deallocate(lat1Af)
-deallocate(lat2Af)
-deallocate(lat1Bf)
-deallocate(lat2Bf)
-deallocate(lat1A_aux)
-deallocate(lat2A_aux)
-!deallocate(lat1B_aux)
-!deallocate(lat2B_aux)
+deallocate(lat1f)
+deallocate(lat2f)
+!deallocate(lat1_aux)
+!deallocate(lat2_aux)
 !deallocate(lat3_aux)
 
 contains
@@ -139,21 +103,20 @@ contains
 function exists_in_lattice(point, lattice) result(flag)
     real*16, dimension(3), intent(in)     :: point
     real*16, dimension(:,:), intent(in)   :: lattice
-    real*16                               :: c1, c2, c3, tol
-    logical, dimension(:), allocatable   :: condition_list
-    logical                              :: flag
-    integer                              :: i, cont
-
-    allocate(condition_list(size(lattice,1)))
+    real*16                               :: tol
+    logical                               :: flag
+    real*16, dimension(3)                 :: vec1
+    logical, dimension(:), allocatable    :: condition_list
+    integer                               :: i
 
     tol = 1.d-3
-    do i=1, size(lattice, 1)
-        c1 = abs(lattice(i,1) - point(1))
-        c2 = abs(lattice(i,2) - point(2))
-        c3 = abs(lattice(i,3) - point(3))
-        condition_list(i) = (c1 < tol) .and. (c2 < tol) .and. (c3 < tol)
-    enddo
+    !flag = any(all(abs(spread(point, dim=1, ncopies=size(lattice,1)) - lattice) .lt. tol, dim=2))
 
+    allocate(condition_list(size(lattice,1)))
+    do i=1, size(lattice, 1)
+        vec1 = abs(lattice(i,:) - point)
+        condition_list(i) = all(vec1 .lt. tol)
+    enddo
     flag = any(condition_list .eqv. .true.)
 end function
 
@@ -199,53 +162,6 @@ end subroutine
 subroutine create_lattice_eh(lattice, z, a, num_columns)
     real*16, dimension(:,:), intent(inout) :: lattice
     real*16, intent(in)                    :: z, a
-    real*16, dimension(3)                  :: v1, v2, v3, v4, origin_a, origin_b, lat_origin
-    real*16                                :: angle, pi, d1
-    integer                                :: i, j, num_columns, row, i0
-
-    pi = 4.e0_16 * atan(1.e0_16)
-    angle = (30.e0_16/180.e0_16) * pi
-    d1 = a/sqrt(3.e0_16)
-
-    v1 = [1.5d0*d1, 0.e0_16, 0.e0_16]
-    v2 = [a*cos(angle), a*sin(angle), 0.e0_16]
-    v3 = [0.e0_16, a, 0.e0_16]
-
-    origin_a = [0.e0_16, 0.e0_16, z]
-    origin_b = origin_a + [d1, 0.e0_16, 0.e0_16]
-
-    row = 1
-    lattice(1,:) = origin_a
-    lattice(2,:) = origin_b
-    i = 3
-    do while(i < size(lattice,1))
-        do j=1, num_columns, 2
-            lattice(i,:) = origin_a + real(j)*v1
-            i = i + 1
-            lattice(i,:) = origin_b + real(j)*v1
-            i = i + 1
-        enddo
-        row  = row + 1
-        if (modulo(row,2) == 1) then
-            origin_a = origin_a + v2
-            origin_b = origin_b + v2
-        else
-            origin_a = origin_a - v2 + v3
-            origin_b = origin_b - v2 + v3
-        endif
-    enddo
-    lattice(1,:) = [0.e0_16, 0.e0_16, z]
-
-    i0 = ((row/2) * num_columns) + (num_columns/2)
-    lat_origin = lattice(i0,:)
-    lattice(:,1) = lattice(:,1) - lat_origin(1)
-    lattice(:,2) = lattice(:,2) - lat_origin(2)
-    lattice(:,3) = lattice(:,3) - lat_origin(3)
-end subroutine
-
-subroutine create_lattice_eh_AB(latticeA, latticeB, z, a, num_columns)
-    real*16, dimension(:,:), intent(inout) :: latticeA, latticeB
-    real*16, intent(in)                    :: z, a
     real*16, dimension(3)                  :: v1, v2, v3, d1, origin_a, origin_b, lat_origin
     real*16                                :: angle, pi, d
     integer                                :: i, j, num_columns, row, i0
@@ -264,13 +180,14 @@ subroutine create_lattice_eh_AB(latticeA, latticeB, z, a, num_columns)
     origin_b = origin_a + d1
 
     row = 1
-    latticeA(1,:) = origin_a
-    latticeB(1,:) = origin_b
-    i = 2
-    do while(i < size(latticeA,1))
+    lattice(1,:) = origin_a
+    lattice(2,:) = origin_b
+    i = 3
+    do while(i < size(lattice,1))
         do j=1, num_columns
-            latticeA(i,:) = origin_a + real(j,16)*v1
-            latticeB(i,:) = origin_b + real(j,16)*v1
+            lattice(i,:) = origin_a + real(j,16)*v1
+            i = i + 1
+            lattice(i,:) = origin_b + real(j,16)*v1
             i = i + 1
         enddo
         row  = row + 1
@@ -282,17 +199,14 @@ subroutine create_lattice_eh_AB(latticeA, latticeB, z, a, num_columns)
             origin_b = origin_b + v3
         endif
     enddo
-    latticeA(1,:) = [0.e0_16, 0.e0_16, z]
-    latticeB(1,:) = [d*cos(angle/2.e0_16), d*sin(angle/2.e0_16), z]
+    lattice(1,:) = [0.e0_16, 0.e0_16, z]
+    lattice(2,:) = [d*cos(angle/2.e0_16), d*sin(angle/2.e0_16), z]
 
     i0 = ((row/2) * num_columns) + (num_columns/2)
-    lat_origin = latticeA(i0,:)
-    latticeA(:,1) = latticeA(:,1) - lat_origin(1)
-    latticeA(:,2) = latticeA(:,2) - lat_origin(2)
-    latticeA(:,3) = latticeA(:,3) - lat_origin(3)
-    latticeB(:,1) = latticeB(:,1) - lat_origin(1)
-    latticeB(:,2) = latticeB(:,2) - lat_origin(2)
-    latticeB(:,3) = latticeB(:,3) - lat_origin(3)
+    lat_origin = lattice(i0,:)
+    lattice(:,1) = lattice(:,1) - lat_origin(1)
+    lattice(:,2) = lattice(:,2) - lat_origin(2)
+    lattice(:,3) = lattice(:,3) - lat_origin(3)
 end subroutine
 
 function rotate_point(point, pivot, angle) result(new_point)
--- plot_lattice.py
+++ plot_lattice.py
@@ -1,41 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open(file="lattice1A.dat") as f:
-    data1A = f.readlines()
+with open(file="lattice01.dat") as f:
+    data1 = f.readlines()
 
-points1A = []
-for line in data1A:
+points1 = []
+for line in data1:
     aux = line.split(";")
-    points1A.append([float(aux[0]), float(aux[1]), float(aux[2])])
-points1A = np.array(points1A)
+    points1.append([float(aux[0]), float(aux[1]), float(aux[2])])
+points1 = np.array(points1)
 
-with open(file="lattice1B.dat") as f:
-    data1B = f.readlines()
+with open(file="lattice02.dat") as f:
+    data2 = f.readlines()
 
-points1B = []
-for line in data1B:
+points2 = []
+for line in data2:
     aux = line.split(";")
-    points1B.append([float(aux[0]), float(aux[1]), float(aux[2])])
-points1B = np.array(points1B)
-
-with open(file="lattice2A.dat") as f:
-    data2A = f.readlines()
-
-points2A = []
-for line in data2A:
-    aux = line.split(";")
-    points2A.append([float(aux[0]), float(aux[1]), float(aux[2])])
-points2A = np.array(points2A)
-
-with open(file="lattice2B.dat") as f:
-    data2B = f.readlines()
-
-points2B = []
-for line in data2B:
-    aux = line.split(";")
-    points2B.append([float(aux[0]), float(aux[1]), float(aux[2])])
-points2B = np.array(points2B)
+    points2.append([float(aux[0]), float(aux[1]), float(aux[2])])
+points2 = np.array(points2)
 
 with open(file="lattice03.dat") as f:
     data3 = f.readlines()
@@ -47,25 +29,17 @@ for line in data3:
 points3 = np.array(points3)
 
 # 2D PLOT (FASTER)
-ax1A = plt.subplot(111)
-graf1A = ax1A.scatter(points1A[:,0], points1A[:,1], s=10)
+ax1 = plt.subplot(111)
+graf1 = ax1.scatter(points1[:,0], points1[:,1], s=10)
 
-ax2A = plt.subplot(111)
-graf2A = ax2A.scatter(points2A[:,0], points2A[:,1], s=10)
-
-ax1B = plt.subplot(111)
-graf1B = ax1B.scatter(points1B[:,0], points1B[:,1], s=10)
-
-ax2B = plt.subplot(111)
-graf2B = ax2B.scatter(points2B[:,0], points2B[:,1], s=10)
+ax2 = plt.subplot(111)
+graf2 = ax2.scatter(points2[:,0], points2[:,1], s=10)
 
 ax3 = plt.subplot(111)
 graf3 = ax3.scatter(points3[:,0], points3[:,1], s=10)
 
-ax1A.set_aspect(1)
-ax2A.set_aspect(1)
-ax1B.set_aspect(1)
-ax2B.set_aspect(1)
+ax1.set_aspect(1)
+ax2.set_aspect(1)
 ax3.set_aspect(1)
 
 # # 3D PLOT (TOO SLOW)
@@ -75,7 +49,7 @@ ax3.set_aspect(1)
 # graf2 = ax2.scatter(points2[:,0], points2[:,1], points2[:,2], s=1)
 # ax3 = plt.subplot(111, projection='3d')
 # graf3 = ax3.scatter(points3[:,0], points3[:,1], points3[:,2], s=1)
-# 
+
 # ax1.set_box_aspect((np.ptp(points1[:,0]), np.ptp(points1[:,1]), np.ptp(points1[:,2])))
 # ax2.set_box_aspect((np.ptp(points2[:,0]), np.ptp(points2[:,1]), np.ptp(points2[:,2])))
 # ax1.azim = 90
