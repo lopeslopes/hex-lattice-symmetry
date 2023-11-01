WIP: OpenMP paralellization works but wrong results

ab_plot.py
@@ -62,7 +62,7 @@ try:
     axAA = plt.subplot(111)
     grafAA = axAA.scatter(pointsAA[:,0], pointsAA[:,1], s=20, color="red")
 except:
-    print("No AA or BB points found")
+    print("No AA points found")
 
 try:
     with open(file="latticeAB.dat") as f:
@@ -76,12 +76,40 @@ try:
     axAB = plt.subplot(111)
     grafAB = axAB.scatter(pointsAB[:,0], pointsAB[:,1], s=20, color="green")
 except:
-    print("No AB or BA points found")
+    print("No AB points found")
+
+try:
+    with open(file="latticeBA.dat") as f:
+        dataBA = f.readlines()
+
+    pointsBA = []
+    for line in dataBA:
+        aux = line.split(";")
+        pointsBA.append([float(aux[0]), float(aux[1]), float(aux[2])])
+    pointsBA = np.array(pointsBA)
+    axBA = plt.subplot(111)
+    grafBA = axBA.scatter(pointsBA[:,0], pointsBA[:,1], s=20, color="purple")
+except:
+    print("No BA points found")
+
+try:
+    with open(file="latticeBB.dat") as f:
+        dataBB = f.readlines()
+
+    pointsBB = []
+    for line in dataBB:
+        aux = line.split(";")
+        pointsBB.append([float(aux[0]), float(aux[1]), float(aux[2])])
+    pointsBB = np.array(pointsBB)
+    axBB = plt.subplot(111)
+    grafBB = axBB.scatter(pointsBB[:,0], pointsBB[:,1], s=20, color="yellow")
+except:
+    print("No BB points found")
 
 axA1.set_aspect(1)
 axB1.set_aspect(1)
 axA2.set_aspect(1)
 axB2.set_aspect(1)
 
-plt.legend(["A1", "B1", "A2", "B2", "AA", "AB"])
+plt.legend(["A1", "B1", "A2", "B2", "AA", "AB", "BA", "BB"])
 plt.show()

organize_angles.py
@@ -1,28 +1,14 @@
 import numpy as np
 
-with open("anglesA.dat") as f:
+with open("angles.dat") as f:
     data_a = f.readlines()
-
-with open("anglesB.dat") as f:
-    data_b = f.readlines()
 
 angles_a = []
 for d in data_a:
     angles_a.append(float(d))
 
-angles_b = []
-for d in data_b:
-    angles_b.append(float(d))
-
 angles_a = sorted(angles_a)
-angles_b = sorted(angles_b)
 
-f = open("treated_anglesA.dat", "w")
 for a in angles_a:
-    if (a <= np.pi/3):
-        f.write(f"{a}\n")
-
-f = open("treated_anglesB.dat", "w")
-for b in angles_b:
-    if (b <= np.pi/3):
-        f.write(f"{b}\n")
+    a_dg = a*(180/np.pi)
+    if (a_dg < 1.2): print(a_dg, a)